4f38d72ebee10428c81ce2ce0f08be3f279299e8
Robinson Roller <robinson@pretto.fr>
2023-05-24 18:15:02 +0200

    update db and add netlify config

--- backend/db.ts
+++ backend/db.ts
@@ -2,9 +2,9 @@ import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 const client = new Client({
   user: "robinson",
-  password: "azerty",
-  database: "eventio",
-  hostname: "localhost",
+  password: "RazZ13MattazZ",
+  database: "postgres",
+  hostname: "planoo.cnj09xyahhy1.us-east-2.rds.amazonaws.com",
   port: 5432,
 });
 
